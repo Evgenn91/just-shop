ab796a59bd4fa02066a96e782e803b89cdca9f55
Evgeniy Nikulin <evgeniy.nikulin.1991@mail.com>
2025-11-10 20:36:59 +0500

    Initial commit

---
+++ src/main/java/evn/petproject/just_shop/JustShopApplication.java
@@ -0,0 +1,13 @@
+package evn.petproject.just_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustShopApplication.class, args);
+	}
+
+}
