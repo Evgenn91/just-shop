ba207573637b9e8d5c3cf0e967b3fa1abb671dcb
Evgeniy Nikulin <evgeniy.nikulin.1991@mail.com>
2025-11-17 23:04:17 +0500

    add validation and json parsing for Product

--- src/main/java/evn/petproject/just_shop/controller/ProductController.java
+++ src/main/java/evn/petproject/just_shop/controller/ProductController.java
@@ -4,6 +4,7 @@ import evn.petproject.just_shop.dto.ProductDto;
 import evn.petproject.just_shop.entity.Product;
 import evn.petproject.just_shop.mapper.ProductMapper;
 import evn.petproject.just_shop.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
     private final ProductService productService;
     private final ProductMapper mapper;
@@ -24,7 +25,7 @@ public class ProductController {
 
     // ---------- CREATE ----------
     @PostMapping
-    public ResponseEntity<ProductDto> create(@RequestBody ProductDto dto) {
+    public ResponseEntity<ProductDto> create(@Valid @RequestBody ProductDto dto) {
         Product entity = mapper.toEntity(dto);
         Product saved = productService.create(entity);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -51,7 +52,7 @@ public class ProductController {
     }
     // ---------- UPDATE (full) ----------
     @PutMapping("/{id}")
-    public ResponseEntity<ProductDto> update(@PathVariable Long id, @RequestBody ProductDto dto) {
+    public ResponseEntity<ProductDto> update(@PathVariable Long id,@Valid @RequestBody ProductDto dto) {
         dto.setId(id);
         Product updated = productService.update(mapper.toEntity(dto));
         return ResponseEntity.ok(mapper.toDto(updated));
--- src/main/java/evn/petproject/just_shop/dto/ProductDto.java
+++ src/main/java/evn/petproject/just_shop/dto/ProductDto.java
@@ -1,5 +1,9 @@
 package evn.petproject.just_shop.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,15 +17,20 @@ import java.time.Instant;
 public class ProductDto {
     private Long id;
 
+    @NotBlank(message = "Название не должно быть пустым")
     private String name;
+    @NotNull(message = "Цена обязательна")
     private BigDecimal price;
     private boolean deleted;
 
+    @PastOrPresent(message = "Дата создания не может быть в будущем")
     private Instant createdAt;
+    @PastOrPresent(message = "Дата создания не может быть в будущем")
     private Instant updatedAt;
 
     private String createdBy;//GUID
     private String modifiedBy;
 
+    @JsonIgnore
     private Long version;
 }
